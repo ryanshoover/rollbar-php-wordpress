c5436e768eab18b56d43a7ea785c726d8c0e598c
Artur Moczulski <artur.moczulski@gmail.com>
2017-11-16 02:31:17 +0000

    Set up the WordPress unit test suite

---
+++ tests/test-rollbarJsConfig.php
@@ -0,0 +1,16 @@
+<?php
+namespace Rollbar\Wordpress\Tests;
+
+/**
+ * Class RollbarJSConfigTest
+ *
+ * @package Rollbar
+ */
+class RollbarJSConfigTest extends \WP_UnitTestCase {
+
+	function test_rollbarJsConfig() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+	
+}
